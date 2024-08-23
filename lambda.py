8685d56a46e753434d13598d9ecd8dd19531460c
6uddychin <75438409+6uddychin@users.noreply.github.com>
2024-08-23 09:12:36 -0700

    updated yaml, lambda and other shit

--- lambda.py
+++ lambda.py
@@ -5,13 +5,35 @@ from datetime import datetime
 
 # Initialize AWS clients
 s3 = boto3.client('s3')
-secrets_manager = boto3.client('secretsmanager')
+# secrets_manager = boto3.client('secretsmanager')
 ses = boto3.client('ses')
 
 # Retrieve the necessary secrets from Secrets Manager
-def get_secret_value(secret_name):
-    response = secrets_manager.get_secret_value(SecretId=secret_name)
-    return response['SecretString']
+def get_secret_value():
+
+    secret_name = "email_automation_secrets"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
+
 
 # Retrieve SES ARN and bucket ARN from Secrets Manager
 SES_ARN = get_secret_value("SES_Arn")
